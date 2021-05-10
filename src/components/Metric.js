c15f3587eead4466ee552d6ae2d72ebb7fc6ea5f
Dragos Strugar <itsdragos@gmail.com>
2021-05-10 20:48:22 +0200

    feat: metric rating UI

--- src/components/Metric.js
+++ src/components/Metric.js
@@ -9,14 +9,35 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 
+import ReactStars from 'react-rating-stars-component'
+
 import {newAccountsPerMonth} from '../config'
 import {metrics} from '../constants'
+
+function RatingFeedback() {
+  return (
+    <div className="w-full px-6 flex justify-around items-center">
+      <span className="text-gray-500">How useful is this metric to you?</span>
+      <ReactStars
+        count={5}
+        size={24}
+        activeColor="#ffd700"
+        onChange={(newRating) => {
+          console.log(newRating)
+          //TODO: make the rating disappear
+          //TODO: make some sort of feedback to the user to indicate successful rating vote
+          //TODO: save the rating in the system for future recommendations
+        }}
+      />
+    </div>
+  )
+}
 
 export default function Metric({kpi}) {
   switch (kpi) {
     case metrics.newAccountsPerMonth:
       return (
-        <div className="w-full h-80">
+        <div className="w-full">
           <ResponsiveContainer>
             <BarChart
               data={newAccountsPerMonth.data}
@@ -24,7 +45,7 @@ export default function Metric({kpi}) {
                 top: 10,
                 right: 30,
                 left: 0,
-                bottom: 0,
+                bottom: 20,
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
@@ -39,6 +60,7 @@ export default function Metric({kpi}) {
               />
             </BarChart>
           </ResponsiveContainer>
+          <RatingFeedback />
         </div>
       )
 
