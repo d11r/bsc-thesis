d22a62758e8bcd76c6622cd78eeb73fadca6fbd0
Dragos Strugar <itsdragos@gmail.com>
2021-05-11 10:56:31 +0200

    feat: more shadow

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export default function Card({children, className}) {
   return (
     <div
-      className={`rounded shadow-lg bg-white py-4 h-72 ${
+      className={`rounded shadow-xl bg-white py-4 h-72 ${
         className ? className : ''
       }`}
     >
